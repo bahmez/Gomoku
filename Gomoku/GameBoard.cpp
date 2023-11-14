0fe8576c38e1f36d0780538329b4a8fdc113cc92
Younes Bahri <91877321+bahmez@users.noreply.github.com>
2023-11-14 22:00:19 +0100

    [Feat] (GameBoard) : add GameBoard

--- Gomoku/GameBoard.cpp
+++ Gomoku/GameBoard.cpp
@@ -5,4 +5,28 @@
 #include "GameBoard.hpp"
 
 namespace Gomoku {
-} // Gomoku
+    GameBoard::GameBoard(unsigned int _width, unsigned int _height):
+    _height(_height),
+    _width(_width) {
+        std::vector<GameBoard::Cell> row;
+
+        row.resize(this->_width, GameBoard::EMPTY);
+        this->_gameBoard.resize(this->_height, row);
+    }
+
+    const std::vector<GameBoard::Cell> &GameBoard::operator[](std::size_t y) const {
+        return this->_gameBoard[y];
+    }
+
+    std::vector<GameBoard::Cell> &GameBoard::operator[](std::size_t y) {
+        return this->_gameBoard[y];
+    }
+
+    unsigned int GameBoard::getWidth() const {
+        return this->_width;
+    }
+
+    unsigned int GameBoard::getHeight() const {
+        return this->_height;
+    }
+}
--- Gomoku/GameBoard.hpp
+++ Gomoku/GameBoard.hpp
@@ -4,8 +4,24 @@
 
 #pragma once
 
+#include <vector>
+
 namespace Gomoku {
     class GameBoard {
-
+    public:
+        enum Cell {
+            EMPTY,
+            PLAYER,
+            OPPONENT
+        };
+        GameBoard(unsigned int _width, unsigned int _height);
+        const std::vector<Cell> &operator[](std::size_t y) const;
+        std::vector<Cell> &operator[](std::size_t y);
+        unsigned int getWidth() const;
+        unsigned int getHeight() const;
+    private:
+        std::vector<std::vector<Cell>> _gameBoard;
+        unsigned int _width;
+        unsigned int _height;
     };
 } // Gomoku
