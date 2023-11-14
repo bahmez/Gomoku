2795db46ffd6047afa608062ca2269376d9a474c
IKEA.luddes <clement.pecas33@gmail.com>
2023-11-14 21:32:26 +0100

    [Feat] (CommandsHandler) : add CommandsHandler

--- CommandsHandler/CommandsHandler.cpp
+++ CommandsHandler/CommandsHandler.cpp
@@ -2,4 +2,31 @@
 // Created by youba on 14/11/2023.
 //
 
+#include <iostream>
+#include <sstream>
 #include "CommandsHandler.hpp"
+
+CommandsHandler::CommandsHandler() {}
+
+void
+CommandsHandler::subscribe(const std::string &name, std::function<void(const std::vector<std::string> &)> function) {
+    this->_events[name] = function;
+}
+
+void CommandsHandler::update() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::stringstream commandStream(line);
+    std::string commandName;
+    std::string argument;
+    std::vector<std::string> arguments;
+
+    commandStream >> commandName;
+    if (!this->_events.contains(commandName))
+        arguments.push_back(commandName);
+    while (commandStream >> argument)
+        arguments.push_back(argument);
+    if (!this->_events.contains(commandName))
+        return this->_events["NotFound"](arguments);
+    this->_events[commandName](arguments);
+}
--- CommandsHandler/CommandsHandler.hpp
+++ CommandsHandler/CommandsHandler.hpp
@@ -4,7 +4,17 @@
 
 #pragma once
 
+#include <map>
+#include <string>
+#include <vector>
+#include <functional>
 
 class CommandsHandler {
-
+public:
+    CommandsHandler();
+    ~CommandsHandler() = default;
+    void subscribe(const std::string &name, std::function<void (const std::vector<std::string> &)> function);
+    void update();
+private:
+    std::map<std::string, std::function<void (const std::vector<std::string> &)>> _events;
 };
